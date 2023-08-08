5a42446eb92693a2e0b71183a17d6925de12edcf
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-08-09 02:35:37 +0300

    update

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,12 +1,12 @@
-import axios from "axios";
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = "BASE_URL";
+axios.defaults.baseURL = 'BASE_URL';
 
-const setAuthHeader = (token) => {
+const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
 const clearAuthHeader = () => {
-  axios.defaults.headers.common.Authorization = "";
+  axios.defaults.headers.common.Authorization = '';
 };
