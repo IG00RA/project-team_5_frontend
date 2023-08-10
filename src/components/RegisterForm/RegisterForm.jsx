af880b1badc4df69b97ab6e74486106cd00856da
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-08-10 08:20:38 +0300

    add registration form

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,3 +1,119 @@
+import React from 'react';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+import {
+  Form,
+  FormTitle,
+  Field,
+  Label,
+  ErrorMessage,
+  Wrapper,
+  Button,
+  LoginIcon,
+} from './RegisterForm.styled';
+
+import sprite from '../../images/svg-sprite/symbol-defs.svg';
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string()
+    .required('Name is required')
+    .matches(/^\S[\S\s]{0,28}\S$/, 'Name must be between 2 and 30 characters')
+    .test(
+      'name-validation',
+      'Name must be at least 2 characters long',
+      value => {
+        return value && value.replace(/\s/g, '').length >= 2;
+      }
+    ),
+  email: Yup.string()
+    .email('This is an ERROR email')
+    .matches(/^[a-zA-Z0-9@.]+$/, 'Email must contain only Latin characters')
+    .required('Email is required'),
+  password: Yup.string()
+    .required('Password is required')
+    .min(6, 'Password must be at least 6 characters long')
+    .matches(/^\S*$/, 'Password must not contain spaces'),
+});
+
 export const RegisterForm = () => {
-  return <p>Register form</p>;
+  return (
+    <Formik
+      initialValues={{ name: '', email: '', password: '' }}
+      validationSchema={validationSchema}
+      onSubmit={(values, actions) => {
+        console.log(values);
+        actions.resetForm();
+      }}
+    >
+      {({ isSubmitting, values, errors, touched, handleSubmit }) => {
+        const isValid = field =>
+          touched[field] && errors[field]
+            ? 'is-invalid'
+            : touched[field]
+            ? 'is-valid'
+            : '';
+
+        return (
+          <Form onSubmit={handleSubmit}>
+            <FormTitle>Sign Up</FormTitle>
+            <Label className={isValid('name')}>
+              Name
+              <Wrapper>
+                <Field
+                  className={isValid('name')}
+                  type="text"
+                  name="name"
+                  placeholder="Enter your name"
+                  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                  value={values.name}
+                />
+              </Wrapper>
+              {isValid('name') === 'is-valid' && <p>This is a CORRECT name</p>}
+              <ErrorMessage name="name" component="div" />
+            </Label>
+            <Label className={isValid('email')}>
+              Email
+              <Wrapper>
+                <Field
+                  className={isValid('email')}
+                  type="email"
+                  name="email"
+                  placeholder="Enter email"
+                  title="Email must be in the format username@domain.com"
+                  value={values.email}
+                />
+              </Wrapper>
+              {isValid('email') === 'is-valid' && (
+                <p>This is a CORRECT email</p>
+              )}
+              <ErrorMessage name="email" component="div" />
+            </Label>
+            <Label className={isValid('password')}>
+              Password
+              <Wrapper>
+                <Field
+                  className={isValid('password')}
+                  type="password"
+                  name="password"
+                  placeholder="Enter password"
+                  title="Password must contain at least one number, one lowercase and one uppercase letter, and be at least 6 characters long."
+                  value={values.password}
+                />
+              </Wrapper>
+              {isValid('password') === 'is-valid' && (
+                <p>This is a CORRECT password</p>
+              )}
+              <ErrorMessage name="password" component="div" />
+            </Label>
+            <Button type="submit" disabled={isSubmitting}>
+              Sign Up
+              <LoginIcon>
+                <use href={sprite + '#icon-log-in'}></use>
+              </LoginIcon>
+            </Button>
+          </Form>
+        );
+      }}
+    </Formik>
+  );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,12 @@
-import { AuthNavigate } from "../components/AuthNavigate/AuthNavigate";
-import { RegisterForm } from "../components/RegisterForm/RegisterForm";
+import { AuthNavigate } from '../components/AuthNavigate/AuthNavigate';
+import { RegisterForm } from '../components/RegisterForm/RegisterForm';
 
 export default function Register() {
   return (
     <div>
       <RegisterForm />
       <AuthNavigate />
+      <div></div>
     </div>
   );
 }
