3357dc9b0ce23d56fc3fe0fa5b8eda0cfd7eb58a
Yaroslav-Marinich <113300647+Yaroslav-Marinich@users.noreply.github.com>
2023-08-10 15:49:02 +0300

    feature/btn_feedback_logout_add_modal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+import Modal from '../components/Modal/Modal';
+import FeedbackButton from '../components/Button/FeedbackButton/FeedbackButton';
+
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div>
       <h1>Welcome to Team 5 Project</h1>
+      <FeedbackButton onClick={openModal} />
+      <Modal isOpen={isModalOpen} onRequestClose={closeModal}>
+        {/* modal content */}
+      </Modal>
     </div>
   );
 }
