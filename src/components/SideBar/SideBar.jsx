360b9cd9bad94c9ece5627192aa0cbe634e845a7
Melnyk675 <113244781+Melnyk675@users.noreply.github.com>
2023-08-11 17:06:39 +0300

    commit

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,12 +1,60 @@
-import { Button } from "../Button/Button";
+import { LogoutBtn } from "../Button/LogoutButton";
+import React from 'react';
 import UserNav from "../UserNav/UserNav";
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
-export default function SideBar() {
+export const SideBar = ({ isModalMenuOpen, closeModalMenu }) => {
+  const dispatch = useDispatch();
+  const handleLogOut = () => dispatch(logOut());
+
+  useEffect(() => {
+    const close = e => {
+      if (e.keyCode === 27) {
+        closeModalMenu(false);
+      }
+    };
+    isModalMenuOpen && window.addEventListener('keydown', close);
+    return () => window.removeEventListener('keydown', close);
+  }, [isModalMenuOpen, closeModalMenu]);
+
+  const handleCloseModalMenu = () => {
+    closeModalMenu(false);
+  };
+
   return (
     <>
-      <h1>Side Bar</h1>
-      <UserNav />
-      <Button />
+      <SideBarWrap className={isModalMenuOpen && 'openModalMenu'}>
+        <TopWrap>
+          <StyledLogoWrapper>
+            <SideBarImg>
+              <source
+                media="(max-width: 767px)"
+              />
+              <source
+                media="(min-width: 768px) and (max-width: 1439px)"
+              />
+              <source
+                media="(min-width: 1440px)"
+              />
+              <img src={logoMobile} alt="Logo goose" />
+            </SideBarImg>
+            <StyledTitle>GooseTrack</StyledTitle>
+          </StyledLogoWrapper>
+          <StyledCloseButton onClick={handleCloseModalMenu}>
+            <CloseIcon />
+          </StyledCloseButton>
+        </TopWrap>
+
+        <UserNav closeModalMenu={closeModalMenu} />
+        <LogoutBtn type="button" onClick={handleLogOut}>
+          {t(`sidebar.Log Out`)}
+          <CgLogIn style={{ marginLeft: 11, width: 20, height: 20 }} />
+        </LogoutBtn>
+      </SideBarWrap>
+      {isModalMenuOpen && <Overlay onClick={handleCloseModalMenu} />}
     </>
   );
-}
+};
+      
+export default SideBar;
