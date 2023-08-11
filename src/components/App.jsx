ed8ad19aa72c4c8ddb40cae0bed5617a77276205
Vladyslav Ushakov <vlad.ushakov.977@gmail.com>
2023-08-11 21:52:23 +0300

    Make account-page and redux auth

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router';
 // import { RestrictedRoute } from "./RestrictedRoute";
 // import { PrivateRoute } from "./PrivateRoute";
@@ -7,18 +7,21 @@ const HomePage = lazy(() => import('../pages/Home'));
 const RegisterPage = lazy(() => import('../pages/Register/Register'));
 const LoginPage = lazy(() => import('../pages/Login/Login'));
 const AccountPage = lazy(() => import('../pages/Account'));
+
 const CalendarPage = lazy(() => import('../pages/Calendar'));
 const StatisticsPage = lazy(() => import('../pages/Statistics'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/account" element={<AccountPage />} />
-      <Route path="/calendar" element={<CalendarPage />} />
-      <Route path="/statistics" element={<StatisticsPage />} />
-    </Routes>
+    <Suspense fallback={null}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/statistics" element={<StatisticsPage />} />
+      </Routes>
+    </Suspense>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './components/GlobalStyles';
 import { App } from './components/App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/project-team_5_frontend">
-    <GlobalStyle />
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/project-team_5_frontend">
+        <GlobalStyle />
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
