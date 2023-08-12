1712d6075a2c9f4a938208aa9bd7caf39f0891a5
Vlasenko1710 <vlasenko_ea@ukr.net>
2023-08-12 22:33:05 +0300

    add_delete_import_header

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -2,7 +2,7 @@ import moment from 'moment';
 import { Suspense, useState } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 import CalendarToolbar from '../components/CalendarToolbar/CalendarToolbar';
-import Header from '../components/Header/Header';
+
 
 export default function Calendar() {
   moment.updateLocale('en', { week: { dow: 1 } });
