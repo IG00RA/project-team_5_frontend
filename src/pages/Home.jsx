9cde1ba57b906de6246f04cef4aa003310660297
Dmytro Kucherenko <110055719+Dmytro-Kucherenko-Kyiv@users.noreply.github.com>
2023-08-14 23:16:09 +0300

    add  adaptive and retina

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
-/* import { AuthSection } from "../components/AuthSection/AuthSection";
-import { Description } from "../components/Description/Description"; */
+import { AuthSection } from "../components/AuthSection/AuthSection";
+import { Description } from "../components/Description/Description";
 
 export default function Home() {
   return (
     <div>
-{/*       <AuthSection/>
-      <Description/> */}
+      <AuthSection/>
+      <Description/>
     </div>
   );
 }
