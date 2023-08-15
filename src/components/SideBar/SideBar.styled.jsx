d9c10d7859f8b6cad0900c9a88431f9d58e35e7c
Melnyk675 <113244781+Melnyk675@users.noreply.github.com>
2023-08-15 16:36:20 +0300

    commit

--- src/components/SideBar/SideBar.styled.jsx
+++ src/components/SideBar/SideBar.styled.jsx
@@ -1,36 +1,31 @@
 import styled from 'styled-components';
 
-
 export const SideBarWrap = styled.div`
+  display: flex;
+  flex-direction: column;
   position: fixed;
-  z-index: 999;
+  z-index: 2;
   box-sizing: border-box;
   height: 100vh;
+  width: 225px;
   padding: 24px 20px;
   background-color: #ffffff;
   border-right: 1px solid;
-  transform: translateX(-100%);
   box-shadow: 0px 0px 7px 1px;
 
-  @media screen and (min-width: 375px) {
-    width: 225px;
-  }
   @media screen and (min-width: 768px) {
     width: 289px;
-    padding: 24px 32px;
+    padding: 32px;
   }
 
   @media screen and (max-width: 1440px) {
+    left: 0;
+    transform: translateX(-100%);
     transition: transform 350ms ease-in-out;
     &.openModalMenu {
+      display: block;
       transform: translateX(0px);
     }
-  }
-
-  @media screen and (min-width: 1440px) {
-    position: relative;
-    transform: translateX(0px);
-    padding: 32px 24px;
   }
 `;
 
