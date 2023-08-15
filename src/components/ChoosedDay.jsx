9ef1d5a22cbc38af6b4a8ab5f3809a8105c1ce6b
Julie Shapovalenko <jools.shapo@gmail.com>
2023-08-15 23:14:51 +0300

    update fetch all tasks to month calendar

--- src/components/ChoosedDay.jsx
+++ src/components/ChoosedDay.jsx
@@ -1,12 +1,12 @@
-import { useParams } from "react-router";
-import { DayCalendarHead } from "./DayCalendarHead/DayCalendarHead";
-import { TasksColumnsList } from "./TasksColumnsList/TasksColumnsList";
-import axios from "axios";
-import moment from "moment";
-import { useEffect, useState } from "react";
+import { useParams } from 'react-router';
+import { DayCalendarHead } from './DayCalendarHead/DayCalendarHead';
+import { TasksColumnsList } from './TasksColumnsList/TasksColumnsList';
+import axios from 'axios';
+import moment from 'moment';
+import { useEffect, useState } from 'react';
 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZDczNThkMDA5YWQ2OTEwN2EzZDNkNSIsImlhdCI6MTY5MjAwODgwOCwiZXhwIjoxNjkyMDkxNjA4fQ.NHUku1fFRQllx9EiHj9H7Yhv1-bzvq8hhtyesn5AH8E";
-axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+// const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZDczNThkMDA5YWQ2OTEwN2EzZDNkNSIsImlhdCI6MTY5MjAwODgwOCwiZXhwIjoxNjkyMDkxNjA4fQ.NHUku1fFRQllx9EiHj9H7Yhv1-bzvq8hhtyesn5AH8E";
+// axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 
 export default function ChoosedDay() {
   const { currentDay } = useParams();
@@ -15,16 +15,24 @@ export default function ChoosedDay() {
 
   useEffect(() => {
     (async () => {
-      const { data } = await axios("https://project-team-5-backend.onrender.com/api/tasks");
-      const filteredTasksByDate = data.filter(task => moment(task.date).format("YYYY:MM:DD") === updatedDate);
+      const { data } = await axios(
+        'https://project-team-5-backend.onrender.com/api/tasks'
+      );
+      const filteredTasksByDate = data.filter(
+        task => moment(task.date).format('YYYY:MM:DD') === updatedDate
+      );
       setTasks(filteredTasksByDate);
     })();
   }, [currentDay, setTasks, updatedDate]);
 
   return (
     <div>
-      <DayCalendarHead currentDay={currentDay} updatedDate={updatedDate} setUpdatedDate={setUpdatedDate} />
+      <DayCalendarHead
+        currentDay={currentDay}
+        updatedDate={updatedDate}
+        setUpdatedDate={setUpdatedDate}
+      />
       <TasksColumnsList tasks={tasks} />
     </div>
   );
-};
+}
