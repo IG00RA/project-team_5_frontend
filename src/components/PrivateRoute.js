b3c3ee347c7e2515687a2d58011550fceaf42a13
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-08-16 09:52:49 +0300

    delete hooks

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,8 +1,10 @@
 import { Navigate } from 'react-router-dom';
-import { useAuth } from 'hooks';
+import { selectIsLoggedIn, selectIsRefreshing } from 'redux/auth/selectors';
+import { useSelector } from 'react-redux';
 
 const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
-  const { isLoggedIn, isRefreshing } = useAuth();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
   const shouldRedirect = !isLoggedIn && !isRefreshing;
 
   return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
