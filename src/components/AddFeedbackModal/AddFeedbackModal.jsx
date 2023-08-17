64004b03ad9e286c803b741a6a828606d281469e
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-08-17 13:49:20 +0300

    add feedbackForm component and base styles for it

--- src/components/AddFeedbackModal/AddFeedbackModal.jsx
+++ src/components/AddFeedbackModal/AddFeedbackModal.jsx
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 // import { useDispatch } from 'react-redux';
 import ModalContainer from '../Modal/ModalConatiner';
+import { FeedbackForm } from 'components/FeedbackForm/FeedbackForm';
 import {
   FeedBackTitle,
   CloseBtn,
@@ -18,7 +19,7 @@ export const AddFeedbackModal = ({ isOpen, onRequestClose, handleClose }) => {
   const [isSubmit] = useState(false);
   return (
     <ModalContainer isOpen={isOpen} onRequestClose={onRequestClose}>
-      <FeedBackForm>
+      {/* <FeedBackForm>
       <FeedBackTitle>Rating</FeedBackTitle>
       <Box className='mask-star-2'/>
       <FeedBackTitle>Review</FeedBackTitle>
@@ -27,8 +28,9 @@ export const AddFeedbackModal = ({ isOpen, onRequestClose, handleClose }) => {
       <FeedBackFormBtn type='submit' className='submit-btn'>{isSubmit === false ? 'Save' : 'Edit'}</FeedBackFormBtn>
       <FeedBackFormBtn type='button' className='cancel-btn' onClick={handleClose}>Cancel</FeedBackFormBtn>
       </Box>
-      </FeedBackForm>
-      <CloseBtn type='button' onClick={handleClose}>
+      </FeedBackForm> */}
+      <FeedbackForm />
+      <CloseBtn type="button" onClick={handleClose}>
         <CloseIcon className="feedBackModalClose" width="24" height="24">
           <use href={svgSprite + `#icon-x-close`} />
         </CloseIcon>
--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,3 +1,93 @@
-export const FeedbackForm = () => {
-  return <p>Feedback Form</p>;
+import React from 'react';
+import { Formik, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+// import { useState } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import sprite from '../../images/svg-sprite/symbol-defs.svg';
+import {
+  Label,
+  InputWrapper,
+  ReviewWrapper,
+  Input,
+  FormBtn,
+  FormBtnWrapper,
+  CancelBtn,
+} from './FeedbackForm.styled';
+
+const validationSchema = Yup.object().shape({
+  rating: Yup.number()
+    .required('Rating is required')
+    .min(1, 'Rating must be at least 1')
+    .max(5, 'Rating must be at most 5'),
+  review: Yup.string()
+    .required('Review is required')
+    .max(300, 'Review must be at most 300 characters'),
+});
+
+export const FeedbackForm = handleClose => {
+  // const dispatch = useDispatch();
+  // const [isEditActive, setIsEditActive] = useState(false);
+
+  return (
+    <Formik
+      initialValues={{
+        rating: '',
+        review: '',
+      }}
+      validationSchema={validationSchema}
+    >
+      <Form>
+        <Label name="rating">Rating</Label>
+        <div className="rating">
+          <input
+            type="radio"
+            name="rating-2"
+            className="mask mask-star-2 bg-orange-400"
+          />
+          <input
+            type="radio"
+            name="rating-2"
+            className="mask mask-star-2 bg-orange-400"
+            checked
+          />
+          <input
+            type="radio"
+            name="rating-2"
+            className="mask mask-star-2 bg-orange-400"
+          />
+          <input
+            type="radio"
+            name="rating-2"
+            className="mask mask-star-2 bg-orange-400"
+          />
+          <input
+            type="radio"
+            name="rating-2"
+            className="mask mask-star-2 bg-orange-400"
+          />
+        </div>
+        <InputWrapper>
+          <ReviewWrapper>
+            <Label htmlFor="review">Review</Label>
+          </ReviewWrapper>
+
+          <Input
+            type="text"
+            placeholder="Enter text"
+            id="review"
+            name="review"
+            component="textarea"
+          />
+          <ErrorMessage name="review" component="div" />
+        </InputWrapper>
+
+        <FormBtnWrapper>
+          <FormBtn type="submit">Save</FormBtn>
+          <CancelBtn type="button" onClick={handleClose}>
+            Cancel
+          </CancelBtn>
+        </FormBtnWrapper>
+      </Form>
+    </Formik>
+  );
 };
