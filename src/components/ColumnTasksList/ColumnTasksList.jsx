0cc6f534445654f6706845621cc9849f1ee279fe
stark1269 <stark1269@gmail.com>
2023-08-17 21:26:21 +0300

    Update ColumnTasksList.jsx

--- src/components/ColumnTasksList/ColumnTasksList.jsx
+++ src/components/ColumnTasksList/ColumnTasksList.jsx
@@ -1,11 +1,8 @@
 import { TaskColumnCard } from "components/TaskColumnCard/TaskColumnCard";
 import { List } from "./ColumnTasksList.styled";
-import { useEffect } from "react";
 
 export const ColumnTasksList = ({ tasksColection, openModal }) => {
   
-  useEffect(() => { }, [tasksColection]);
-
   return (
     <List>
       {tasksColection?.map(task => (
