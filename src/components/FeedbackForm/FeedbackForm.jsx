aa5ed134c4cbb40f2332b94d331c818eaea83a94
DmytroSadovskyi <dmytro.sadovskyi@gmail.com>
2023-08-17 20:41:25 +0300

    add some logic

--- src/components/FeedbackForm/FeedbackForm.jsx
+++ src/components/FeedbackForm/FeedbackForm.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Formik, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-// import { useState } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import sprite from '../../images/svg-sprite/symbol-defs.svg';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import sprite from '../../images/svg-sprite/symbol-defs.svg';
+import { selectRating, selectReview } from 'redux/review/reviewSelectors';
+import {
+  addReview,
+  updateReview,
+  removeReview,
+} from 'redux/review/reviewOperations';
 import {
   Label,
   InputWrapper,
@@ -12,6 +18,9 @@ import {
   FormBtn,
   FormBtnWrapper,
   CancelBtn,
+  EditWrapper,
+  EditBtn,
+  DeleteBtn,
 } from './FeedbackForm.styled';
 
 const validationSchema = Yup.object().shape({
@@ -25,69 +34,114 @@ const validationSchema = Yup.object().shape({
 });
 
 export const FeedbackForm = ({ handleClose }) => {
-  // const dispatch = useDispatch();
-  // const [isEditActive, setIsEditActive] = useState(false);
+  const dispatch = useDispatch();
+  const userReview = useSelector(selectReview);
+  const userRating = useSelector(selectRating);
+  const [isEditActive, setIsEditActive] = useState(false);
+
+  const handleSubmit = () => {};
+
+  const handleEdit = () => {
+    setIsEditActive(!isEditActive);
+  };
+
+  const handleDelete = () => {};
+
+  const RatingComponent = ({ value }) => {
+    const maxRating = 5;
+    const ratingArray = Array.from(
+      { length: maxRating },
+      (_, index) => index + 1
+    );
+
+    return (
+      <div className="rating">
+        {ratingArray.map(ratingValue => (
+          <input
+            key={ratingValue}
+            type="radio"
+            name="rating"
+            className="mask mask-star-2 bg-orange-400"
+            checked={ratingValue === value}
+          />
+        ))}
+      </div>
+    );
+  };
 
   return (
     <Formik
       initialValues={{
-        rating: '',
-        review: '',
+        rating: userRating || null,
+        review: userReview || '',
       }}
       validationSchema={validationSchema}
+      onSubmit={handleSubmit}
     >
-      <Form>
-        <Label name="rating">Rating</Label>
-        <div className="rating">
-          <input
-            type="radio"
-            name="rating-2"
-            className="mask mask-star-2 bg-orange-400"
-          />
-          <input
-            type="radio"
-            name="rating-2"
-            className="mask mask-star-2 bg-orange-400"
-            checked
-          />
-          <input
-            type="radio"
-            name="rating-2"
-            className="mask mask-star-2 bg-orange-400"
-          />
-          <input
-            type="radio"
-            name="rating-2"
-            className="mask mask-star-2 bg-orange-400"
-          />
-          <input
-            type="radio"
-            name="rating-2"
-            className="mask mask-star-2 bg-orange-400"
-          />
-        </div>
-        <InputWrapper>
-          <ReviewWrapper>
-            <Label htmlFor="review">Review</Label>
-          </ReviewWrapper>
+      {({ values }) => (
+        <Form>
+          <Label name="rating">Rating</Label>
+          <RatingComponent value={values.rating} />
+          <InputWrapper>
+            <ReviewWrapper>
+              <Label htmlFor="review">Review</Label>
 
-          <Input
-            type="text"
-            placeholder="Enter text"
-            id="review"
-            name="review"
-            component="textarea"
-          />
-          <ErrorMessage name="review" component="div" />
-        </InputWrapper>
+              {userReview !== '' && (
+                <EditWrapper>
+                  <EditBtn
+                    onClick={handleEdit}
+                    isActive={isEditActive}
+                    type="button"
+                  >
+                    <svg
+                      style={{
+                        width: '16px',
+                        height: '16px',
+                        stroke: '#FFFFFF',
+                        fill: 'none',
+                      }}
+                    >
+                      <use href={sprite + '#icon-pencil'}></use>
+                    </svg>
+                  </EditBtn>
+                  <DeleteBtn type="button" onClick={handleDelete}>
+                    <svg
+                      style={{
+                        width: '16px',
+                        height: '16px',
+                        stroke: '#EA3D65',
+                        fill: '#EA3D65',
+                        strokeWidth: '1.5',
+                      }}
+                    >
+                      <use href={sprite + '#icon-trash-box'}></use>
+                    </svg>
+                  </DeleteBtn>
+                </EditWrapper>
+              )}
+            </ReviewWrapper>
+            <Input
+              type="text"
+              placeholder="Enter text"
+              id="review"
+              name="review"
+              component="textarea"
+              disabled={!isEditActive && userReview !== ''}
+            />
 
-        <FormBtnWrapper>
-          <FormBtn type="submit">Save</FormBtn>
-          <CancelBtn type="button" onClick={handleClose}>
-            Cancel
-          </CancelBtn>
-        </FormBtnWrapper>
-      </Form>
+            <ErrorMessage name="review" component="div" />
+          </InputWrapper>
+
+          {(userReview !== '' || isEditActive) && (
+            <FormBtnWrapper>
+              <FormBtn type="submit">{isEditActive ? 'Edit' : 'Save'}</FormBtn>
+              <CancelBtn type="button" onClick={handleClose}>
+                Cancel
+              </CancelBtn>
+            </FormBtnWrapper>
+          )}
+        </Form>
+      )}
     </Formik>
   );
 };
