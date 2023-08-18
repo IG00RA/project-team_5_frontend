9072456e21d6d31489a89359f4fe450c7c09284a
Kateryna Verveda <katushka211@gmail.com>
2023-08-18 08:55:39 +0200

    add selectFilteredTasksByMonth

--- src/redux/tasks/tasksSelectors.js
+++ src/redux/tasks/tasksSelectors.js
@@ -1,7 +1,15 @@
-import moment from "moment";
+import moment from 'moment';
 
 export const selectFilteredTasksByDate = (tasks, date) => {
-  return tasks?.filter(task => moment(task?.date).format("YYYY-MM-DD") === date)
+  return tasks?.filter(task => moment(task.date).format('YYYY-MM-DD') === date);
+};
+
+export const selectFilteredTasksByMonth = (tasks, date) => {
+  return tasks?.filter(
+    task =>
+      moment(task.date).startOf('month').format('YYYY-MM-DD') <= date &&
+      moment(task.date).clone().endOf('month').format('YYYY-MM-DD') >= date
+  );
 };
 
 export const selectTasks = state => state.tasks.tasks;
