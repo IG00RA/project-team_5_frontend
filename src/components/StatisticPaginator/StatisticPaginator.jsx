9b67486236ac34a4c7b902fc336ad51e0732cdcd
Julie Shapovalenko <jools.shapo@gmail.com>
2023-08-19 02:08:47 +0300

    styling of datepicker

--- src/components/StatisticPaginator/StatisticPaginator.jsx
+++ src/components/StatisticPaginator/StatisticPaginator.jsx
@@ -1,40 +1,67 @@
-import {
-  BtnNext,
-  BtnPrev,
-  PaginatorWrapper,
-  Period,
-} from './StatisticPaginator.styled';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-export default function StatisticPaginator({
-  selectedDate,
-  onDateChange,
-  typeOfPeriod,
-}) {
-  const formattedPeriod =
-    typeOfPeriod === 'month'
-      ? selectedDate.format('MMMM YYYY')
-      : selectedDate.format('D MMMM YYYY');
-  const prevHandler = () => {
-    if (typeOfPeriod === 'month') {
-      onDateChange(prev => prev.clone().subtract(1, 'month'));
-    } else if (typeOfPeriod === 'day') {
-      onDateChange(prev => prev.clone().subtract(1, 'day'));
-    }
-  };
-  const nextHandler = () => {
-    if (typeOfPeriod === 'month') {
-      onDateChange(prev => prev.clone().add(1, 'month'));
-    } else if (typeOfPeriod === 'day') {
-      onDateChange(prev => prev.clone().add(1, 'day'));
-    }
-  };
+import // BtnNext,
+// BtnPrev,
+// PaginatorWrapper,
+// Period,
+'./StatisticPaginator.styled';
+import { forwardRef, useState } from 'react';
+import { DatePickerWrapper, Period } from './StatisticPaginator.styled';
+
+const StatisticPaginator = () => {
+  const [startDate, setStartDate] = useState(new Date());
+
+  const CustomInput = forwardRef(({ value, onClick }, ref) => (
+    <Period onClick={onClick} ref={ref}>
+      {value}
+    </Period>
+  ));
+
   return (
-    <PaginatorWrapper>
-      <Period>{formattedPeriod}</Period>
-      <div>
-        <BtnPrev onClick={prevHandler}>&lt;</BtnPrev>
-        <BtnNext onClick={nextHandler}>&gt;</BtnNext>
-      </div>
-    </PaginatorWrapper>
+    <DatePickerWrapper>
+      <DatePicker
+        dateFormat="dd MMM yyyy"
+        selected={startDate}
+        onChange={date => setStartDate(date)}
+        customInput={<CustomInput />}
+      />
+    </DatePickerWrapper>
   );
-}
+};
+
+export default StatisticPaginator;
+
+// export default function StatisticPaginator({
+//   selectedDate,
+//   onDateChange,
+//   typeOfPeriod,
+// }) {
+//   const formattedPeriod =
+//     typeOfPeriod === 'month'
+//       ? selectedDate.format('MMMM YYYY')
+//       : selectedDate.format('D MMMM YYYY');
+//   const prevHandler = () => {
+//     if (typeOfPeriod === 'month') {
+//       onDateChange(prev => prev.clone().subtract(1, 'month'));
+//     } else if (typeOfPeriod === 'day') {
+//       onDateChange(prev => prev.clone().subtract(1, 'day'));
+//     }
+//   };
+//   const nextHandler = () => {
+//     if (typeOfPeriod === 'month') {
+//       onDateChange(prev => prev.clone().add(1, 'month'));
+//     } else if (typeOfPeriod === 'day') {
+//       onDateChange(prev => prev.clone().add(1, 'day'));
+//     }
+//   };
+//   return (
+//     <PaginatorWrapper>
+//       <Period>{formattedPeriod}</Period>
+//       <div>
+//         <BtnPrev onClick={prevHandler}>&lt;</BtnPrev>
+//         <BtnNext onClick={nextHandler}>&gt;</BtnNext>
+//       </div>
+//     </PaginatorWrapper>
+//   );
+// }
--- src/components/StatisticPaginator/StatisticPaginator.styled.jsx
+++ src/components/StatisticPaginator/StatisticPaginator.styled.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+// import DatePicker from 'react-datepicker';
 
 export const PaginatorWrapper = styled.div`
   display: flex;
@@ -30,28 +31,117 @@ export const Period = styled.div`
   }
 `;
 
-export const BtnNext = styled.button`
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
+export const DatePickerWrapper = styled.div`
+  .react-datepicker {
+    background-color: #3e85f3;
+    /* color: white; */
 
-  width: 36px;
-  height: 30px;
-  padding: 7px 10px;
-  font-weight: 700;
-  border-radius: 0px 8px 8px 0px;
-  border: 1px solid rgba(220, 227, 229, 0.8);
-  background-color: #fff;
-  color: ${p =>
-    p.disabled ? 'rgba(220, 227, 229, 1)' : 'rgba(52, 52, 52, 1)'};
+    border-radius: 16px;
 
-  @media screen and (min-width: 768px) {
-    width: 38px;
-    height: 34px;
+    /* width: 327px;
+    height: 354px; */
   }
+
+  .react-datepicker__day--weekend {
+    color: rgba(255, 255, 255, 0.2);
+  }
+
+  .react-datepicker__header {
+  }
+
+  .react-datepicker__month-container {
+    padding: 9px 18px;
+  }
+
+  .react-datepicker__triangle {
+    display: none;
+  }
+
+  .react-datepicker__month {
+    background-color: #3e85f3;
+  }
+
+  .react-datepicker__header {
+    background-color: #3e85f3;
+  }
+
+  .react-datepicker__current-month {
+    color: white;
+  }
+
+  .react-datepicker__day-name {
+    color: white;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin-top: 10px;
+  }
+
+  .react-datepicker__day {
+    border-radius: 50%;
+    color: white;
+  }
+
+  /* .react-datepicker .react-datepicker__day--weekend {
+    background-color: rgba(255, 255, 255, 0.4);
+  } */
+
+  .react-datepicker__day--selected {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+
+    border-radius: 50%;
+    background-color: rgba(255, 255, 255, 1);
+    color: #3e85f3;
+    outline: #fff;
+  }
+
+  /* .react-datepicker__day--today {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+
+    border-radius: 50%;
+    background-color: rgba(255, 255, 255, 1);
+    color: #3e85f3;
+    outline: #fff;
+  } */
+
+  .react-datepicker__day
+    .react-datepicker__day--selected
+    .react-datepicker__day--today
+    .react-datepicker__day--weekend {
+  }
+
+  /* .react-datepicker__day-names,
+  .react-datepicker__week {
+  } */
+
+  span
+    .react-datepicker__navigation-icon
+    .react-datepicker__navigation-icon--next::before {
+    content: 'Next Month';
+    position: relative;
+    top: 15px;
+    right: 15px;
+    color: #fff;
+    stroke: #fff;
+    fill: currentColor;
+  }
+  /* 
+  .react-datepicker .react-datepicker__day--weekend {
+    color: ${p => (p.selected ? '#3e85f3' : 'rgba(255, 255, 255, 0.4)')};
+  } */
+
+  .react-datepicker__day .eact-datepicker__day--outside-month {
+  }
+
+  /* @media screen and (min-width: 768px) {
+   
+  } */
 `;
 
-export const BtnPrev = styled.button`
+export const PaginatorBtn = styled.button`
   display: inline-flex;
   justify-content: center;
   align-items: center;
@@ -60,11 +150,16 @@ export const BtnPrev = styled.button`
   height: 30px;
   padding: 7px 10px;
   font-weight: 700;
-  border-radius: 0px 8px 8px 0px;
+  border-radius: ${p => (p.$isPrevBtn ? '8px 0px 0px 8px' : '0px 8px 8px 0px')};
   border: 1px solid rgba(220, 227, 229, 0.8);
   background-color: #fff;
   color: ${p =>
     p.disabled ? 'rgba(220, 227, 229, 1)' : 'rgba(52, 52, 52, 1)'};
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+
+  &:hover {
+    box-shadow: 0px 1px 7px 1px rgba(0, 0, 0, 0.1);
+  }
 
   @media screen and (min-width: 768px) {
     width: 38px;
