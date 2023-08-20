ac08148437c9a14a4323b4c2781592aacbce3455
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-08-20 09:46:23 +0300

    fix review error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import FeedbackButton from '../components/Buttons/FeedbackButton/FeedbackButton';
-import ModalContainer from '../components/Modal/ModalConatiner';
+// import React, { useState, useEffect } from 'react';
+// import FeedbackButton from '../components/Buttons/FeedbackButton/FeedbackButton';
+// import ModalContainer from '../components/Modal/ModalConatiner';
 import { AuthSection } from '../components/AuthSection/AuthSection';
 import { Description } from '../components/Description/Description';
-import ReviewsSlider from 'components/ReviewsSlider/ReviewsSlider'; 
-import ScrollToTop from 'components/ScrollToTop/ScrollToTop';
-import { AddFeedbackModal } from '../components/AddFeedbackModal/AddFeedbackModal';
+// import ReviewsSlider from 'components/ReviewsSlider/ReviewsSlider';
+// import ScrollToTop from 'components/ScrollToTop/ScrollToTop';
+// import { AddFeedbackModal } from '../components/AddFeedbackModal/AddFeedbackModal';
+// import { fetchReviewsData } from 'redux/review/reviewOperations';
 
 export default function Home() {
-  const [isModalOpen1, setIsModalOpen1] = useState(false);
-  const [isModalOpen2, setIsModalOpen2] = useState(false);
+  // const [isModalOpen1, setIsModalOpen1] = useState(false);
+  // const [isModalOpen2, setIsModalOpen2] = useState(false);
 
-  const openModal1 = () => {
-    setIsModalOpen1(true);
-  };
-  const openModal2 = () => {
-    setIsModalOpen2(true);
-  };
+  // const openModal1 = () => {
+  //   setIsModalOpen1(true);
+  // };
+  // const openModal2 = () => {
+  //   setIsModalOpen2(true);
+  // };
 
-  const closeModal1 = () => {
-    setIsModalOpen1(false);
-  };
-  const closeModal2 = () => {
-    setIsModalOpen2(false);
-  };
+  // const closeModal1 = () => {
+  //   setIsModalOpen1(false);
+  // };
+  // const closeModal2 = () => {
+  //   setIsModalOpen2(false);
+  // };
 
-  const [reviewskData, setReviewsData] = useState([]);
+  // const [reviewsData, setReviewsData] = useState([]);
 
-  useEffect(() => {
-    fetchReviewsData()
-      .then((data) => setReviewsData(data))
-      .catch((error) => console.error('Error fetching reviews:', error));
-  }, []);
+  // useEffect(() => {
+  //   fetchReviewsData()
+  //     .then(data => setReviewsData(data))
+  //     .catch(error => console.error('Error fetching reviews:', error));
+  // }, []);
 
   return (
     <>
       <div>
         <AuthSection />
         <Description />
-        <ReviewsSlider reviewsData={reviewskData}/>
-        <ScrollToTop />
-
+        {/* <ReviewsSlider reviewsData={reviewsData} /> */}
+        {/* <ScrollToTop /> */}
+        {/* 
         <FeedbackButton onClick={openModal1} />
         <FeedbackButton onClick={openModal2} />
         <AddFeedbackModal isOpen={isModalOpen1} onRequestClose={closeModal1} />
         <ModalContainer isOpen={isModalOpen2} onRequestClose={closeModal2}>
           <p>HELLO2</p>
-        </ModalContainer>
+        </ModalContainer> */}
       </div>
     </>
   );
