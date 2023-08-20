e2d48948931816be77034652633857a7c9f8caad
Dmytro Kucherenko <110055719+Dmytro-Kucherenko-Kyiv@users.noreply.github.com>
2023-08-20 23:42:43 +0300

    fix img

--- src/components/Description/Description.styled.jsx
+++ src/components/Description/Description.styled.jsx
@@ -152,6 +152,7 @@ export const Image = styled.img`
   background: ${p => p.theme.colors.homePage.imageBg};
   margin-top: 40px;
   text-align: center;
+  object-fit: cover;
 
   @media screen and (min-width: 375px) {
     width: 704px;
