2d72b952494d6bcf0cd4e55dd70affe9c39806cf
stark1269 <stark1269@gmail.com>
2023-08-20 12:13:19 +0300

    fix style

--- src/components/MonthCalendarHead/MonthCalendarHead.styled.jsx
+++ src/components/MonthCalendarHead/MonthCalendarHead.styled.jsx
@@ -4,22 +4,24 @@ export const WeekDaysWrapper = styled.ul`
   display: grid;
   grid-template-columns: repeat(7, 1fr);
   grid-gap: 1px;
-  background-color: ${props => props.theme.colors.calendarPage.mainBg};
-  border: 1px solid ${props => props.theme.colors.calendarPage.border};
-  border-radius: 8px;
-  text-align: center;
+
   padding: 8px;
   margin-bottom: 14px;
+  width: 100%;
+
+  border-radius: 8px;
+  text-align: center;
+  
+  background-color: ${props => props.theme.colors.calendarPage.mainBg};
+  border: 1px solid ${props => props.theme.colors.calendarPage.border};
 
   @media screen and (min-width: 375px) {
     padding: 16px;
   }
+
   @media screen and (min-width: 768px) {
     padding: 14px;
     margin-bottom: 18px;
-  }
-  @media screen and (min-width: 1440px) {
-    max-width: 1067px;
   }
 `;
 
