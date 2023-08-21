cfa28b3e4637cab4f3603af20a70e783cca841c0
Julie Shapovalenko <jools.shapo@gmail.com>
2023-08-21 15:18:42 +0300

    fix day of the week appearence

--- src/components/MonthCalendarHead/MonthCalendarHead.jsx
+++ src/components/MonthCalendarHead/MonthCalendarHead.jsx
@@ -5,14 +5,26 @@ const MonthCalendarHead = ({ startDay }) => {
   const day = startDay.clone().subtract(1, 'day');
   const daysOfWeek = [...Array(7)].map(() => day.add(1, 'day').clone());
 
+  const currentWidth = window.innerWidth;
+  const viewport =
+    currentWidth < 768 ? 'mobile' : currentWidth < 1440 ? 'laptop' : 'desctop';
+
+  const setFormat = idx => {
+    return viewport === 'mobile'
+      ? moment()
+          .day(idx + 1)
+          .format('dd')[0]
+      : moment()
+          .day(idx + 1)
+          .format('ddd');
+  };
+
   return (
     <WeekDaysWrapper>
       {daysOfWeek.map((dayItem, idx) => (
         <li key={dayItem.unix()}>
           <WeekDay $isWeekendDay={dayItem.day() === 6 || dayItem.day() === 0}>
-            {moment()
-              .day(idx + 1)
-              .format('ddd')}
+            {setFormat(idx)}
           </WeekDay>
         </li>
       ))}
