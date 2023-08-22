d48e7ca49e031d0aac0deaeeb6fde7b6d2141d45
Julie Shapovalenko <jools.shapo@gmail.com>
2023-08-22 17:45:43 +0300

    update scroll

--- src/components/ColumnTasksList/ColumnTasksList.styled.jsx
+++ src/components/ColumnTasksList/ColumnTasksList.styled.jsx
@@ -6,21 +6,21 @@ export const List = styled.ul`
   gap: 18px;
 
   height: 100%;
+  width: 100%;
 
   overflow: hidden;
   overflow-y: scroll;
   scrollbar-width: 2px;
-
-  /* &::-webkit-scrollbar {
+  scrol &::-webkit-scrollbar {
     width: 0;
     height: 0;
-  } */
+  }
 
   // полоса прокрутки (скроллбар)
 
   &::-webkit-scrollbar {
-    width: 0.35rem;
-    border: 0.0625rem solid rgba(0, 0, 0, 0.237);
+    width: 0.4rem;
+    border: 0.0625rem solid rgba(105, 104, 104, 0.237);
     background-color: transparent;
   }
 
@@ -29,10 +29,14 @@ export const List = styled.ul`
   &::-webkit-scrollbar-thumb {
     background-color: ${props => props.theme.colors.modal.task.taskScroll};
     border-radius: 0.2rem;
-    box-shadow: 2px 1px 9px 0px rgba(136, 165, 191, 0.48);
+    border: 1px solid rgba(105, 104, 104, 0.237);
   }
 
   &::-webkit-scrollbar-thumb:hover {
     background-color: ${props => props.theme.colors.modal.task.taskScrollHover};
+  }
+
+  &::-webkit-scrollbar-track {
+    margin-left: 2px;
   }
 `;
