2b4377fcd8c5b07c83d20626731227f1dae787f7
Kateryna Verveda <katushka211@gmail.com>
2023-09-24 19:42:07 +0200

    add settings for i18n

---
+++ src/i18n.js
@@ -0,0 +1,16 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    fallbackLng: 'en',
+    lng: localStorage.getItem('i18nextLng') || 'en',
+  });
+
+export default i18n;
